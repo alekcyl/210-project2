890f8b8da7f95fb2ab9a7166dc9b0c355f4b0409
alekcyl <alekcylwik@gmail.com>
2022-11-18 12:22:35 -0500

    added demo mechanics

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    public GameObject Missile;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject shot = Instantiate(Missile, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.Euler(new Vector3(0,0,90)));
+
+            
+            shot.GetComponent<Rigidbody2D>().AddForce(new Vector2(1000, 0));
+
+        }
+
+        float movementInput = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.position += new Vector3(0, movementInput, 0);
+
+        
+    }
+
+}
